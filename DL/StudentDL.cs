6a19bf35e9159a577546e22064a2c277062df0a3
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-03-18 19:25:29 +0500

    Add CRUD operations for Student entity

--- DL/StudentDL.cs
+++ DL/StudentDL.cs
@@ -25,5 +25,27 @@ namespace MID.DL
             query = query.Replace("@RegNo", RegNo);
             return DBConfig.ExecuteCommand(query);
         }
+        public static int DeleteStudent(string RegistrationNumber)
+        {
+            string query = "DELETE FROM Student WHERE RegistrationNumber = '@RegNo'";
+            query = query.Replace("@RegNo", RegistrationNumber);
+            return DBConfig.ExecuteCommand(query);
+        }
+        public static int UpdateStudent(string FirstName, string LastName, string Contact, string Email, string RegNo)
+        {
+            string query = "UPDATE Student SET FirstName = '@FirstName', LastName = '@LastName', Contact = '@Contact', Email = '@Email' WHERE RegistrationNumber = '@RegNo'";
+            query = query.Replace("@FirstName", FirstName);
+            query = query.Replace("@LastName", LastName);
+            query = query.Replace("@Contact", Contact);
+            query = query.Replace("@Email", Email);
+            query = query.Replace("@RegNo", RegNo);
+            return DBConfig.ExecuteCommand(query);
+        }
+        public static DataTable SearchStudent(string RegistrationNumber)
+        {
+            string query = "SELECT * FROM Student WHERE RegistrationNumber = '@RegNo'";
+            query = query.Replace("@RegNo", RegistrationNumber);
+            return DBConfig.GetData(query);
+        }
     }
 }
