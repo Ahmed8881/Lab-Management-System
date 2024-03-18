4ce5f9f3a3dee51a206dcfbeb09d335290140646
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-03-18 19:14:44 +0500

    Add methods to retrieve and add student data

--- DL/StudentDL.cs
+++ DL/StudentDL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace MID.DL
 {
     internal class StudentDL
     {
+        private static DBConfig DBConfig = new DBConfig();
+        public static DataTable GetStudentData()
+        {
+            string query = "SELECT * FROM Student";
+            return DBConfig.GetData(query);
+        }
+        public static int AddStudent(string FirstName, string LastName, string Contact, string Email, string RegNo)
+        {
+            string query = "Insert into Student(FirstName, LastName, Contact, Email, RegistrationNumber) values('@FirstName', '@LastName', '@Contact', '@Email', '@RegNo')";
+            query = query.Replace("@FirstName", FirstName);
+            query = query.Replace("@LastName", LastName);
+            query = query.Replace("@Contact", Contact);
+            query = query.Replace("@Email", Email);
+            query = query.Replace("@RegNo", RegNo);
+            return DBConfig.ExecuteCommand(query);
+        }
     }
 }
