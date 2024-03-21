d4c6e7ca9c52dd7e4c0fa5b41afc5895d65f8dfc
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-03-21 21:56:09 +0500

    Change Layout of Rubric Level page

--- Forms/Rubric Level.Designer.cs
+++ Forms/Rubric Level.Designer.cs
@@ -30,14 +30,14 @@
         {
             label1 = new Label();
             button1 = new Button();
-            textBox1 = new TextBox();
             label2 = new Label();
             textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
             button2 = new Button();
             button3 = new Button();
             dataGridView1 = new DataGridView();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -45,10 +45,10 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label1.Location = new Point(50, 77);
+            label1.Location = new Point(44, 58);
             label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(95, 28);
+            label1.Size = new Size(77, 21);
             label1.TabIndex = 0;
             label1.Text = "Rubric ID";
             // 
@@ -57,73 +57,44 @@
             button1.BackColor = SystemColors.Highlight;
             button1.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
             button1.ForeColor = SystemColors.ButtonHighlight;
-            button1.Location = new Point(443, 536);
-            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Location = new Point(388, 402);
+            button1.Margin = new Padding(2);
             button1.Name = "button1";
-            button1.Size = new Size(115, 55);
+            button1.Size = new Size(101, 41);
             button1.TabIndex = 1;
             button1.Text = "ADD";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(201, 77);
-            textBox1.Margin = new Padding(2, 2, 2, 2);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(138, 27);
-            textBox1.TabIndex = 2;
-            // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label2.Location = new Point(50, 131);
+            label2.Location = new Point(44, 98);
             label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(136, 28);
+            label2.Size = new Size(110, 21);
             label2.TabIndex = 3;
             label2.Text = "Rubric Details";
             label2.Click += label2_Click;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(201, 135);
-            textBox2.Margin = new Padding(2, 2, 2, 2);
+            textBox2.Location = new Point(176, 101);
+            textBox2.Margin = new Padding(2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(138, 27);
+            textBox2.Size = new Size(121, 23);
             textBox2.TabIndex = 6;
-            // 
-            // textBox3
-            // 
-            textBox3.ForeColor = Color.Gray;
-            textBox3.Location = new Point(61, 192);
-            textBox3.Margin = new Padding(2, 2, 2, 2);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(260, 27);
-            textBox3.TabIndex = 7;
-            textBox3.Text = "Level Details";
-            textBox3.TextChanged += textBox3_TextChanged;
-            // 
-            // textBox4
-            // 
-            textBox4.ForeColor = Color.Gray;
-            textBox4.Location = new Point(61, 248);
-            textBox4.Margin = new Padding(2, 2, 2, 2);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(260, 27);
-            textBox4.TabIndex = 8;
-            textBox4.Text = "Measurement Details";
             // 
             // button2
             // 
             button2.BackColor = SystemColors.Highlight;
             button2.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
             button2.ForeColor = SystemColors.ButtonHighlight;
-            button2.Location = new Point(614, 536);
-            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Location = new Point(537, 402);
+            button2.Margin = new Padding(2);
             button2.Name = "button2";
-            button2.Size = new Size(106, 55);
+            button2.Size = new Size(93, 41);
             button2.TabIndex = 9;
             button2.Text = "UPDATE";
             button2.UseVisualStyleBackColor = false;
@@ -133,10 +104,10 @@
             button3.BackColor = SystemColors.Highlight;
             button3.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
             button3.ForeColor = SystemColors.ButtonHighlight;
-            button3.Location = new Point(757, 536);
-            button3.Margin = new Padding(2, 2, 2, 2);
+            button3.Location = new Point(662, 402);
+            button3.Margin = new Padding(2);
             button3.Name = "button3";
-            button3.Size = new Size(118, 55);
+            button3.Size = new Size(103, 41);
             button3.TabIndex = 10;
             button3.Text = "DELETE";
             button3.UseVisualStyleBackColor = false;
@@ -145,29 +116,56 @@
             // 
             dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(396, 46);
-            dataGridView1.Margin = new Padding(2, 2, 2, 2);
+            dataGridView1.Location = new Point(346, 34);
+            dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(568, 456);
+            dataGridView1.Size = new Size(497, 342);
             dataGridView1.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(176, 60);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(176, 145);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
+            label3.Location = new Point(44, 147);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 21);
+            label3.TabIndex = 14;
+            label3.Text = "Rubric Level";
             // 
             // Rubric_Level
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(985, 749);
+            ClientSize = new Size(862, 562);
+            Controls.Add(label3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
             Controls.Add(dataGridView1);
             Controls.Add(button3);
             Controls.Add(button2);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(label2);
-            Controls.Add(textBox1);
             Controls.Add(button1);
             Controls.Add(label1);
-            Margin = new Padding(2, 2, 2, 2);
+            Margin = new Padding(2);
             Name = "Rubric_Level";
             Text = "Rubric_Level";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -179,13 +177,13 @@
 
         private Label label1;
         private Button button1;
-        private TextBox textBox1;
         private Label label2;
         private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
         private Button button2;
         private Button button3;
         private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label3;
     }
 }
