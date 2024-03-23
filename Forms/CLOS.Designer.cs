c90fb311c5e5e737d4c7a9953b3c056f8762dd1d
Ahmed8881 <buttahmed2004@gmail.com>
2024-03-23 13:59:37 +0500

    Add Icons TO clo

--- Forms/CLOS.Designer.cs
+++ Forms/CLOS.Designer.cs
@@ -67,7 +67,7 @@
             dataGridView1.Margin = new Padding(2, 3, 2, 3);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(407, 355);
+            dataGridView1.Size = new Size(559, 485);
             dataGridView1.TabIndex = 2;
             dataGridView1.CellClick += dataGridView1_CellClick;
             // 
@@ -77,10 +77,12 @@
             ADDCLO.BackColor = SystemColors.Highlight;
             ADDCLO.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
             ADDCLO.ForeColor = SystemColors.ControlLightLight;
-            ADDCLO.Location = new Point(312, 463);
+            ADDCLO.Image = Properties.Resources.icons8_add_50;
+            ADDCLO.ImageAlign = ContentAlignment.MiddleLeft;
+            ADDCLO.Location = new Point(339, 605);
             ADDCLO.Margin = new Padding(2, 3, 2, 3);
             ADDCLO.Name = "ADDCLO";
-            ADDCLO.Size = new Size(93, 45);
+            ADDCLO.Size = new Size(170, 60);
             ADDCLO.TabIndex = 7;
             ADDCLO.Text = "ADD";
             ADDCLO.UseVisualStyleBackColor = false;
@@ -92,10 +94,12 @@
             UPDATECLO.BackColor = SystemColors.Highlight;
             UPDATECLO.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
             UPDATECLO.ForeColor = SystemColors.ControlLightLight;
-            UPDATECLO.Location = new Point(446, 463);
+            UPDATECLO.Image = Properties.Resources.icons8_update_done_24;
+            UPDATECLO.ImageAlign = ContentAlignment.MiddleLeft;
+            UPDATECLO.Location = new Point(533, 605);
             UPDATECLO.Margin = new Padding(2, 3, 2, 3);
             UPDATECLO.Name = "UPDATECLO";
-            UPDATECLO.Size = new Size(95, 45);
+            UPDATECLO.Size = new Size(154, 60);
             UPDATECLO.TabIndex = 8;
             UPDATECLO.Text = "UPDATE";
             UPDATECLO.UseVisualStyleBackColor = false;
@@ -107,10 +111,12 @@
             DELETECLO.BackColor = SystemColors.Highlight;
             DELETECLO.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
             DELETECLO.ForeColor = SystemColors.ControlLightLight;
-            DELETECLO.Location = new Point(591, 463);
+            DELETECLO.Image = Properties.Resources.icons8_delete_48__1_;
+            DELETECLO.ImageAlign = ContentAlignment.MiddleLeft;
+            DELETECLO.Location = new Point(733, 605);
             DELETECLO.Margin = new Padding(2, 3, 2, 3);
             DELETECLO.Name = "DELETECLO";
-            DELETECLO.Size = new Size(88, 45);
+            DELETECLO.Size = new Size(175, 60);
             DELETECLO.TabIndex = 9;
             DELETECLO.Text = "DELETE";
             DELETECLO.UseVisualStyleBackColor = false;
@@ -140,7 +146,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(767, 532);
+            ClientSize = new Size(919, 714);
             Controls.Add(DatePicker);
             Controls.Add(label1);
             Controls.Add(DELETECLO);
