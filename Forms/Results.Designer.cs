bb496ce4640bb40594e06eadc28c0b7049e3dacf
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-03-24 07:58:00 +0500

    Update Results Page

--- Forms/Results.Designer.cs
+++ Forms/Results.Designer.cs
@@ -37,12 +37,10 @@
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
-            label6 = new Label();
             comboBox2 = new ComboBox();
             comboBox3 = new ComboBox();
             comboBox4 = new ComboBox();
             comboBox5 = new ComboBox();
-            comboBox6 = new ComboBox();
             label7 = new Label();
             dataGridView1 = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -51,26 +49,28 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(197, 43);
+            comboBox1.Location = new Point(172, 32);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(111, 28);
+            comboBox1.Size = new Size(98, 23);
             comboBox1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label1.Location = new Point(45, 39);
+            label1.Location = new Point(39, 29);
             label1.Name = "label1";
-            label1.Size = new Size(84, 28);
+            label1.Size = new Size(68, 21);
             label1.TabIndex = 1;
             label1.Text = "Student";
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(812, 45);
+            dateTimePicker1.Location = new Point(393, 140);
+            dateTimePicker1.Margin = new Padding(3, 2, 3, 2);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(263, 27);
+            dateTimePicker1.Size = new Size(231, 23);
             dateTimePicker1.TabIndex = 2;
             // 
             // button1
@@ -80,9 +80,10 @@
             button1.ForeColor = SystemColors.ControlLightLight;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(781, 131);
+            button1.Location = new Point(683, 98);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(198, 70);
+            button1.Size = new Size(173, 52);
             button1.TabIndex = 3;
             button1.Text = "Evalute";
             button1.UseVisualStyleBackColor = false;
@@ -91,9 +92,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label2.Location = new Point(45, 109);
+            label2.Location = new Point(39, 82);
             label2.Name = "label2";
-            label2.Size = new Size(120, 28);
+            label2.Size = new Size(97, 21);
             label2.TabIndex = 4;
             label2.Text = "Component";
             // 
@@ -101,9 +102,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label3.Location = new Point(43, 187);
+            label3.Location = new Point(38, 140);
             label3.Name = "label3";
-            label3.Size = new Size(122, 28);
+            label3.Size = new Size(99, 21);
             label3.TabIndex = 5;
             label3.Text = "Rubric Level";
             // 
@@ -111,9 +112,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label4.Location = new Point(364, 40);
+            label4.Location = new Point(318, 30);
             label4.Name = "label4";
-            label4.Size = new Size(120, 28);
+            label4.Size = new Size(96, 21);
             label4.TabIndex = 6;
             label4.Text = "Assessment";
             // 
@@ -121,69 +122,55 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label5.Location = new Point(364, 112);
+            label5.Location = new Point(318, 84);
             label5.Name = "label5";
-            label5.Size = new Size(127, 28);
+            label5.Size = new Size(103, 21);
             label5.TabIndex = 7;
             label5.Text = "Rubric Detail";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label6.Location = new Point(364, 190);
-            label6.Name = "label6";
-            label6.Size = new Size(146, 28);
-            label6.TabIndex = 8;
-            label6.Text = "Rubric Level Id";
             // 
             // comboBox2
             // 
             comboBox2.FormattingEnabled = true;
-            comboBox2.Location = new Point(197, 116);
+            comboBox2.Location = new Point(172, 87);
+            comboBox2.Margin = new Padding(3, 2, 3, 2);
             comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(111, 28);
+            comboBox2.Size = new Size(98, 23);
             comboBox2.TabIndex = 9;
             // 
             // comboBox3
             // 
             comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(197, 187);
+            comboBox3.Location = new Point(172, 140);
+            comboBox3.Margin = new Padding(3, 2, 3, 2);
             comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(111, 28);
+            comboBox3.Size = new Size(98, 23);
             comboBox3.TabIndex = 10;
             // 
             // comboBox4
             // 
             comboBox4.FormattingEnabled = true;
-            comboBox4.Location = new Point(526, 44);
+            comboBox4.Location = new Point(460, 33);
+            comboBox4.Margin = new Padding(3, 2, 3, 2);
             comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(111, 28);
+            comboBox4.Size = new Size(98, 23);
             comboBox4.TabIndex = 11;
             // 
             // comboBox5
             // 
             comboBox5.FormattingEnabled = true;
-            comboBox5.Location = new Point(526, 116);
+            comboBox5.Location = new Point(460, 87);
+            comboBox5.Margin = new Padding(3, 2, 3, 2);
             comboBox5.Name = "comboBox5";
-            comboBox5.Size = new Size(111, 28);
+            comboBox5.Size = new Size(98, 23);
             comboBox5.TabIndex = 12;
-            // 
-            // comboBox6
-            // 
-            comboBox6.FormattingEnabled = true;
-            comboBox6.Location = new Point(526, 191);
-            comboBox6.Name = "comboBox6";
-            comboBox6.Size = new Size(111, 28);
-            comboBox6.TabIndex = 13;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Yu Gothic UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic);
-            label7.Location = new Point(708, 45);
+            label7.Location = new Point(318, 138);
             label7.Name = "label7";
-            label7.Size = new Size(54, 28);
+            label7.Size = new Size(44, 21);
             label7.TabIndex = 14;
             label7.Text = "Date";
             // 
@@ -191,25 +178,24 @@
             // 
             dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(45, 246);
+            dataGridView1.Location = new Point(39, 184);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(1010, 325);
+            dataGridView1.Size = new Size(884, 244);
             dataGridView1.TabIndex = 15;
             // 
             // Results
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1087, 604);
+            ClientSize = new Size(951, 453);
             Controls.Add(dataGridView1);
             Controls.Add(label7);
-            Controls.Add(comboBox6);
             Controls.Add(comboBox5);
             Controls.Add(comboBox4);
             Controls.Add(comboBox3);
             Controls.Add(comboBox2);
-            Controls.Add(label6);
             Controls.Add(label5);
             Controls.Add(label4);
             Controls.Add(label3);
@@ -218,6 +204,7 @@
             Controls.Add(dateTimePicker1);
             Controls.Add(label1);
             Controls.Add(comboBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Results";
             Text = "Results";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -235,12 +222,10 @@
         private Label label3;
         private Label label4;
         private Label label5;
-        private Label label6;
         private ComboBox comboBox2;
         private ComboBox comboBox3;
         private ComboBox comboBox4;
         private ComboBox comboBox5;
-        private ComboBox comboBox6;
         private Label label7;
         private DataGridView dataGridView1;
     }
