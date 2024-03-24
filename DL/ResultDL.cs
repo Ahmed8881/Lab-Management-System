9f27fff412c417b40fb49effa21f9f28fe59d70d
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-03-24 08:08:20 +0500

    Add ResultDL class with functions to fill combo boxes

---
+++ DL/ResultDL.cs
@@ -0,0 +1,63 @@
+using MID.DL;
+using System.Data;
+
+class ResultDL
+{
+    private static DBConfig dBConfig = new();
+    // Functions to Fill combo boxes
+    public static List<string> GetStudentRegNos()
+    {
+        string query = "SELECT RegistrationNumber FROM Student";
+        DataTable dt = dBConfig.GetData(query);
+        List<string> regNos = new();
+        foreach (DataRow dr in dt.Rows)
+        {
+            regNos.Add(dr["RegistrationNumber"].ToString());
+        }
+        return regNos;
+    }
+    public static List<string> GetAssessmentTitle()
+    {
+        string query = "SELECT Title FROM Assessment";
+        DataTable dt = dBConfig.GetData(query);
+        List<string> titles = new();
+        foreach (DataRow dr in dt.Rows)
+        {
+            titles.Add(dr["Title"].ToString());
+        }
+        return titles;
+    }
+    public static List<string> GetAssessmentComponentNames(string AssessmentTitle)
+    {
+        string query = "SELECT Name FROM AssessmentComponent WHERE AssessmentId = (SELECT Id FROM Assessment WHERE Title = '" + AssessmentTitle + "')";
+        DataTable dt = dBConfig.GetData(query);
+        List<string> names = new();
+        foreach (DataRow dr in dt.Rows)
+        {
+            names.Add(dr["Name"].ToString());
+        }
+        return names;
+    }
+    public static List<string> GetRubricDetails(string ComponentName)
+    {
+        string query = "SELECT Details FROM Rubric WHERE Id = (SELECT RubricId FROM AssessmentComponent WHERE Name = '" + ComponentName + "')";
+        DataTable dt = dBConfig.GetData(query);
+        List<string> details = new();
+        foreach (DataRow dr in dt.Rows)
+        {
+            details.Add(dr["Details"].ToString());
+        }
+        return details;
+    }
+    public static List<int> GetRubricMeasurementLevel(string RubricDetails)
+    {
+        string query = "SELECT MeasurementLevel FROM RubricLevel WHERE RubricId = (SELECT Id FROM Rubric WHERE Details = '" + RubricDetails + "')";
+        DataTable dt = dBConfig.GetData(query);
+        List<int> levels = new();
+        foreach (DataRow dr in dt.Rows)
+        {
+            levels.Add(Convert.ToInt32(dr["MeasurementLevel"]));
+        }
+        return levels;
+    }
+}
